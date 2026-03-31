6872fd6083cea51b898b9aafcea56b35a00703cd
Sitcka <snapaspuei@gmail.com>
2026-03-31 13:49:07 +0200

    Inicializacion de Git

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GestorTareas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
